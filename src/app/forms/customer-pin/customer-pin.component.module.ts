f74120562e164ff1b19a89a425541ae20eac295f
ghevariyaarjun <ghevariyaarjun@gmail.com>
2022-10-11 21:54:13 +0530

    setup ng2-file-upload

--- src/app/forms/customer-pin/customer-pin.component.module.ts
+++ src/app/forms/customer-pin/customer-pin.component.module.ts
@@ -1,15 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { CustomerPinComponent } from './customer-pin.component';
+
+import { MatInputModule } from '@angular/material/input';
+
+import { NgxSelectModule } from 'ngx-select-ex';
+
+import { FileUploadModule } from 'ng2-file-upload';
+
 
 
 @NgModule({
-    imports:      [ CommonModule , FormsModule, MatDialogModule],
-    declarations: [ CustomerPinComponent  ],
-    exports:      [ CustomerPinComponent  ]
-   })
-   export class CustomerPinComponentModule { }
-
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatInputModule,
+    NgxSelectModule,
+    FileUploadModule
+  ],
+  declarations: [CustomerPinComponent],
+  exports: [CustomerPinComponent],
+})
+export class CustomerPinComponentModule {}
--- src/app/forms/customer-pin/customer-pin.component.ts
+++ src/app/forms/customer-pin/customer-pin.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FileUploader } from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = 'http://localhost:3000/api/';
 
 @Component({
   selector: 'app-customer-pin',
   templateUrl: './customer-pin.component.html',
-  styleUrls: ['./customer-pin.component.scss']
+  styleUrls: ['./customer-pin.component.scss'],
 })
-export class CustomerPinComponent implements OnInit {
+export class CustomerPinComponent {
+  addCustomerForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+    collaboratory: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  hasAnotherDropZoneOver: boolean;
+  response: string;
 
-  ngOnInit(): void {
+  constructor() {
+    this.uploader = new FileUploader({
+      url: URL,
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async (item: any) => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date(),
+          });
+        });
+      },
+    });
+
+    this.hasBaseDropZoneOver = false;
+    this.hasAnotherDropZoneOver = false;
+
+    this.response = '';
+
+    this.uploader.response.subscribe((res) => (this.response = res));
   }
 
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
+
+  callingFunction() {}
+
+  fileSelected(ev: any) {
+    console.log(ev);
+  }
 }
