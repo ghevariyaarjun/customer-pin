37e7e2d8f3ca86f627c78c3e97b81d0c1635cade
ghevariyaarjun <ghevariyaarjun@gmail.com>
2022-10-11 20:56:07 +0530

    Create customer form added

--- src/app/forms/create-customer/create-customer.component.module.ts
+++ src/app/forms/create-customer/create-customer.component.module.ts
@@ -1,16 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { CreateCustomerComponent } from './create-customer.component'
+import { CreateCustomerComponent } from './create-customer.component';
 
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
+import { MatInputModule } from '@angular/material/input';
 
+import { NgxSelectModule } from 'ngx-select-ex';
+
+ 
 @NgModule({
-    imports:      [ CommonModule , FormsModule, MatDialogModule],
-    declarations: [ CreateCustomerComponent  ],
-    exports:      [ CreateCustomerComponent  ]
-   })
-   export class CreateCustomerComponentModule { }
-
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatInputModule,
+    NgxSelectModule
+  ],
+  declarations: [CreateCustomerComponent],
+  exports: [CreateCustomerComponent],
+})
+export class CreateCustomerComponentModule {}
--- src/app/forms/create-customer/create-customer.component.ts
+++ src/app/forms/create-customer/create-customer.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+import regionAPI from './country';
 
 @Component({
   selector: 'app-create-customer',
   templateUrl: './create-customer.component.html',
-  styleUrls: ['./create-customer.component.scss']
+  styleUrls: ['./create-customer.component.scss'],
 })
 export class CreateCustomerComponent implements OnInit {
+  addCustomerForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    region: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  regionList: any[] = Object.values(regionAPI.data)
+    .map(({ region }) => region)
+    .filter((v, i, a) => a.indexOf(v) === i);
 
-  ngOnInit(): void {
+  countryList: any = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  callingFunction() {}
+
+  regionOptionChange(ev: any) {
+    if (ev[0] && ev[0].value) {
+      this.countryList = Object.values(regionAPI.data)
+        .filter(({ region }) => region === ev[0].value)
+        .map(({ country }) => country);
+    } else {
+      this.countryList = [];
+    }
   }
-
 }
