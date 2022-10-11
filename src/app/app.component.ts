6ed854879ecf1f72d01944de415e9630db83a7c3
ghevariyaarjun <ghevariyaarjun@gmail.com>
2022-10-11 19:11:28 +0530

    Sample Test Data Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,36 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+ 
+export interface PeriodicElement {
+  image: string;
+  title: number;
+  collaboratory: number;
+  privacy: 'Public' | 'Private';
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {title: 1, image: 'Hydrogen', collaboratory: 1.0079, privacy: 'Public'},
+  {title: 2, image: 'Helium', collaboratory: 4.0026, privacy: 'Public'},
+  {title: 3, image: 'Lithium', collaboratory: 6.941, privacy: 'Public'},
+  {title: 4, image: 'Beryllium', collaboratory: 9.0122, privacy: 'Public'},
+  {title: 5, image: 'Boron', collaboratory: 10.811, privacy: 'Public'},
+  {title: 6, image: 'Carbon', collaboratory: 12.0107, privacy: 'Public'},
+  {title: 7, image: 'Nitrogen', collaboratory: 14.0067, privacy: 'Public'},
+  {title: 8, image: 'Oxygen', collaboratory: 15.9994, privacy: 'Public'},
+  {title: 9, image: 'Fluorine', collaboratory: 18.9984, privacy: 'Public'},
+  {title: 10, image: 'Neon', collaboratory: 20.1797, privacy: 'Public'},
+  {title: 11, image: 'Sodium', collaboratory: 22.9897, privacy: 'Public'},
+  {title: 12, image: 'Magnesium', collaboratory: 24.305, privacy: 'Public'},
+  {title: 13, image: 'Aluminum', collaboratory: 26.9815, privacy: 'Public'},
+  {title: 14, image: 'Silicon', collaboratory: 28.0855, privacy: 'Public'},
+  {title: 15, image: 'Phosphorus', collaboratory: 30.9738, privacy: 'Public'},
+  {title: 16, image: 'Sulfur', collaboratory: 32.065, privacy: 'Public'},
+  {title: 17, image: 'Chlorine', collaboratory: 35.453, privacy: 'Public'},
+  {title: 18, image: 'Argon', collaboratory: 39.948, privacy: 'Public'},
+  {title: 19, image: 'Potassium', collaboratory: 39.0983, privacy: 'Public'},
+  {title: 20, image: 'Calcium', collaboratory: 40.078, privacy: 'Public'},
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +38,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'customer_pins';
+  displayedColumns: string[] = ['title', 'image', 'collaboratory', 'privacy'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
